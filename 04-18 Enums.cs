83a6f8196a423f5aace1cef4daf4faeada625390
Ylonah <ylonahbe@gmail.com>
2026-04-18 13:38:13 +0800

    Create 04-18 Enums.cs

---
+++ 04-18 Enums.cs
@@ -0,0 +1,36 @@
+namespace ISPRGG_SHNGPRL_FINALS_TCG
+{
+    public enum CardType
+    {
+        FIRE,
+        WATER,
+        GRASS
+    }
+
+    public enum StatusEffect
+    {
+        None,
+        Burned,
+        Confused,
+        Poisoned
+    }
+
+    public enum NationalIndex
+    {
+        Pyro = 1,
+        Moss = 2,
+        Dew = 3,
+        Flare = 4,
+        Fern = 5,
+        Mist = 6,
+        Scorch = 7,
+        Vine = 8,
+        Tide = 9,
+        Cinder = 10,
+        Sprout = 11,
+        Ripple = 12,
+        Blaze = 13,
+        Bloom = 14,
+        Brook = 15
+    }
+}
